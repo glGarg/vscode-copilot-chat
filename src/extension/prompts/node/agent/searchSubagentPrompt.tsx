6cf157937359e92bcf1c97be231158f6ff8aecca
root <root@aks-a100ib-42183938-vmss000004.oquuyr03ufxezo4c4x3thsxcth.zrhx.internal.cloudapp.net>
2025-12-11 20:26:13 +0000

    align prompts by adding a system prompt

--- src/extension/prompts/node/agent/searchSubagentPrompt.tsx
+++ src/extension/prompts/node/agent/searchSubagentPrompt.tsx
@@ -2,6 +2,9 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+
+
+
 import { PromptElement, PromptSizing, SystemMessage, UserMessage } from '@vscode/prompt-tsx';
 import { GenericBasePromptElementProps } from '../../../context/node/resolvers/genericPanelIntentInvocation';
 import { CopilotToolMode } from '../../../tools/common/toolsRegistry';
--- src/extension/tools/node/searchSubagentTool.ts
+++ src/extension/tools/node/searchSubagentTool.ts
@@ -44,6 +44,8 @@ class SearchSubagentTool implements ICopilotTool<ISearchSubagentParams> {
 			`${options.input.details}`,
 			'',
 		].join('\n');
+		// they have a simple query in sft data.
+		const searchInstruction = `Find relevant code snippets for: ${options.input.query}`;
 
 		const loop = this.instantiationService.createInstance(SubagentToolCallingLoop, {
 			toolCallLimit: 5,
